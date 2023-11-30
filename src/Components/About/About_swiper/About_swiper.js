46ecdc5ae5a08c833899a938befd508c1a9d7853
Sadriddin <axhmadullayev2006@gmail.com>
2023-11-30 11:09:04 +0500

    second commit

--- src/Components/About/About_swiper/About_swiper.js
+++ src/Components/About/About_swiper/About_swiper.js
@@ -4,13 +4,24 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import 'swiper/css/effect-fade'
+import 'swiper/css/scrollbar';
 
 import './About_swiper.css';
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Scrollbar, EffectFade } from 'swiper/modules';
+
+//Images import
+import school1 from "../../../Images/1.jpg"
+import school2 from "../../../Images/2.jpg"
+import school3 from "../../../Images/3.jpg"
+import school4 from "../../../Images/4.jpg"
+import school5 from "../../../Images/5.jpg"
+import school6 from "../../../Images/6.jpg"
+import school7 from "../../../Images/7.jpg"
+import school8 from "../../../Images/8.jpg"
+import school9 from "../../../Images/9.jpg"
 
 export default function App() {
     const progressCircle = useRef(null);
@@ -23,28 +34,34 @@ export default function App() {
         <>
             <Swiper
                 spaceBetween={30}
-                centeredSlides={true}
+                effect={'fade'}
+                // centeredSlides={true}
                 autoplay={{
-                    delay: 2500,
+                    delay: 5500,
                     disableOnInteraction: false,
                 }}
-                pagination={{
-                    clickable: true,
+                // pagination={{
+                //     clickable: true,
+                // }}
+                scrollbar={{
+                    hide: true,
                 }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-                onAutoplayTimeLeft={onAutoplayTimeLeft}
+                // navigation={true}
+
+                modules={[Autoplay, Scrollbar, EffectFade]}
+                // onAutoplayTimeLeft={onAutoplayTimeLeft}
                 className="mySwiper"
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                <SwiperSlide><img src={school1} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={school2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={school3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={school4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={school5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={school6} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={school7} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={school8} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={school9} alt="" /></SwiperSlide>
+
                 <div className="autoplay-progress" slot="container-end">
                     <svg viewBox="0 0 48 48" ref={progressCircle}>
                         <circle cx="24" cy="24" r="20"></circle>
